f572cd96ec4b0827d19f86c1dc662c456d43087d
frankfang <frankfang1990@gmail.com>
2020-01-16 18:50:25 +0800

    active-class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -39,6 +39,9 @@
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected{
+      color: red;
     }
   }
 </style>
