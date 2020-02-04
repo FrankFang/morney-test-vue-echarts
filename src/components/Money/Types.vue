6c1a1310cf1486803ae39455f4134bdae9929d90
你e <frankfang1990@gmail.com>
2020-02-04 17:11:58 +0800

    安装 vue-class-component 的文档，使用 props 失败

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -11,27 +11,51 @@
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'Types',
-    props: ['xxx'],
-    data() {
-      return {
-        type: '-' // '-'表示支出，'+'表示收入
-      }
-    },
-    mounted() {
-      console.log(this.xxx)
-    },
-    methods: {
-      selectType(type) { // type 只能是 '-' 和 '+' 中的一个
-        if (type !== '-' && type !== '+') {
-          throw new Error('type is unknown')
-        }
-        this.type = type
-      }
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component({
+    props: {
+      propMessage: String
     }
+  })
+  export default class Types extends Vue {
+    type = '-'; // '-'表示支出，'+'表示收入
+    helloMsg = 'Hello, ' + this.propMessage;
+
+    selectType(type: string) { // type 只能是 '-' 和 '+' 中的一个
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown');
+      }
+      this.type = type;
+    }
+
+    created() {}
+
+    mounted() {}
   }
+
+  // export default {
+  //   name: 'Types',
+  //   props: ['xxx'],
+  //   data() {
+  //     return {
+  //       type: '-' // '-'表示支出，'+'表示收入
+  //     }
+  //   },
+  //   mounted() {
+  //     console.log(this.xxx)
+  //   },
+  //   methods: {
+  //     selectType(type) { // type 只能是 '-' 和 '+' 中的一个
+  //       if (type !== '-' && type !== '+') {
+  //         throw new Error('type is unknown')
+  //       }
+  //       this.type = type
+  //     }
+  //   }
+  // }
 </script>
 
 <style lang="scss" scoped>
