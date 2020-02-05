87122a534ceac9d18b1294f515202ac6aa3090f2
你e <frankfang1990@gmail.com>
2020-02-05 15:25:41 +0800

    完成 Types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -13,49 +13,18 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
-  @Component({
-    props: {
-      propMessage: String
-    }
-  })
+  @Component
   export default class Types extends Vue {
     type = '-'; // '-'表示支出，'+'表示收入
-    helloMsg = 'Hello, ' + this.propMessage;
-
-    selectType(type: string) { // type 只能是 '-' 和 '+' 中的一个
+    selectType(type: string) {
       if (type !== '-' && type !== '+') {
         throw new Error('type is unknown');
       }
       this.type = type;
     }
-
-    created() {}
-
-    mounted() {}
   }
-
-  // export default {
-  //   name: 'Types',
-  //   props: ['xxx'],
-  //   data() {
-  //     return {
-  //       type: '-' // '-'表示支出，'+'表示收入
-  //     }
-  //   },
-  //   mounted() {
-  //     console.log(this.xxx)
-  //   },
-  //   methods: {
-  //     selectType(type) { // type 只能是 '-' 和 '+' 中的一个
-  //       if (type !== '-' && type !== '+') {
-  //         throw new Error('type is unknown')
-  //       }
-  //       this.type = type
-  //     }
-  //   }
-  // }
 </script>
 
 <style lang="scss" scoped>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad/>
-    <Types xxx="hi"/>
+    <Types :xxx="    333     "/>
     <Notes/>
     <Tags/>
   </Layout>
