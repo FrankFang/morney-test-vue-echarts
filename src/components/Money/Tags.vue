2d04e57357972c3534ca5cc162cfa6ff8230fecf
你e <frankfang1990@gmail.com>
2020-02-05 20:36:38 +0800

    完成 Tags 组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag"
@@ -19,7 +19,7 @@
 
   @Component
   export default class Tags extends Vue {
-    @Prop() dataSource: string[] | undefined;
+    @Prop() readonly dataSource: string[] | undefined;
     selectedTags: string[] = [];
 
     toggle(tag: string) {
@@ -29,6 +29,17 @@
       } else {
         this.selectedTags.push(tag);
       }
+    }
+
+    create() {
+      const name = window.prompt('请输入标签名');
+      if (name === '') {
+        window.alert('标签名不能为空');
+      } else if (this.dataSource) {
+        this.$emit('update:dataSource',
+          [...this.dataSource, name]);
+      }
+
     }
   }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,7 +3,7 @@
     <NumberPad/>
     <Types/>
     <Notes/>
-    <Tags :data-source="tags"/>
+    <Tags :data-source.sync="tags"/>
   </Layout>
 </template>
 
