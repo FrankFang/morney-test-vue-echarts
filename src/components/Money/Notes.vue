8b404393de7c962425ade33b2eeb1e41e6166d58
你e <frankfang1990@gmail.com>
2020-02-05 17:30:38 +0800

    完成 notes 组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,15 +2,26 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入备注">
+      <input type="text" :value="value"
+             @input="onInput"
+             placeholder="在这里输入备注">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue {
+    value = '';
+
+    onInput(event: KeyboardEvent) {
+      const input = event.target as HTMLInputElement;
+      this.value = input.value;
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
