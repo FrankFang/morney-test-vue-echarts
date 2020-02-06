5114b4ea4184beff4fdee127bb63cca5e4603567
你e <frankfang1990@gmail.com>
2020-02-06 21:34:49 +0800

    将 tag 的 string 改为 {id, name}

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -29,7 +29,7 @@
       } else {
         this.selectedTags.push(tag);
       }
-      this.$emit('update:value', this.selectedTags)
+      this.$emit('update:value', this.selectedTags);
     }
 
     create() {
--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,11 @@
 const localStorageKeyName = 'tagList';
+type Tag = {
+  id: string;
+  name: string;
+}
 type TagListModel = {
-  data: string[]
-  fetch: () => string[]
+  data: Tag[]
+  fetch: () => Tag[]
   create: (name: string) => 'success' | 'duplicated' // 联合类型
   save: () => void
 }
@@ -12,8 +16,10 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name) {
-    if (this.data.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push(name);
+    // this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push({id: name, name: name});
     this.save();
     return 'success';
   },
