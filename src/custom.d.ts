c328d23c14f1196bb9e58fe10b21ca24010d0a11
你e <frankfang1990@gmail.com>
2020-02-11 16:37:50 +0800

    封装 record 的操作

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -25,4 +25,6 @@ interface Window {
   findTag: (id: string) => Tag | undefined;
   removeTag: (id: string) => boolean;
   updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  recordList: RecordItem[];
+  createRecord: (record: RecordItem) => void
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,18 +18,14 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
-
-  const recordList = recordListModel.fetch();
-
+  import {Component} from 'vue-property-decorator';
 
   @Component({
     components: {Tags, FormItem, Types, NumberPad}
   })
   export default class Money extends Vue {
     tags = window.tagList;
-    recordList: RecordItem[] = recordList;
+    recordList = window.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -43,12 +39,7 @@
     }
 
     saveRecord() {
-      recordListModel.create(this.record);
-    }
-
-    @Watch('recordList')
-    onRecordListChange() {
-      recordListModel.save();
+      window.createRecord(this.record);
     }
   }
 </script>
