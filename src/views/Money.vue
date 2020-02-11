97e8bc6fb86cc87b4227eddd1c732d870f505173
你e <frankfang1990@gmail.com>
2020-02-11 11:40:15 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -45,14 +45,12 @@
     }
 
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+      recordListModel.create(this.record);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
