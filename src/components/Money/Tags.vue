174d17cf06e4d607e51250ce5adc903620008970
frankfang <frankfang1990@gmail.com>
2020-02-17 15:34:42 +0800

    Labels.vue 使用 Vuex，抽离 TagHelper 作为 mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in tagList" :key="tag.id"
@@ -16,6 +16,8 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component, Prop} from 'vue-property-decorator';
+  import {mixins} from 'vue-class-component';
+  import TagHelper from '@/mixins/TagHelper';
 
   @Component({
     computed: {
@@ -24,7 +26,7 @@
       }
     }
   })
-  export default class Tags extends Vue {
+  export default class Tags extends mixins(TagHelper) {
     selectedTags: string[] = [];
 
     created() {
@@ -39,12 +41,6 @@
         this.selectedTags.push(tag);
       }
       this.$emit('update:value', this.selectedTags);
-    }
-
-    create() {
-      const name = window.prompt('请输入标签名');
-      if (!name) { return window.alert('标签名不能为空'); }
-      this.$store.commit('createTag', name);
     }
   }
 </script>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -22,20 +22,21 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Button from '@/components/Button.vue';
+  import {mixins} from 'vue-class-component';
+  import TagHelper from '@/mixins/TagHelper';
+
 
   @Component({
-    components: {Button}
+    components: {Button},
+    computed: {
+      tags() {
+        return this.$store.state.tagList;
+      },
+    }
   })
-  export default class Labels extends Vue {
-    //TODO
-    tags = []; // store.tagList; // 知识点1
-
-    createTag() {
-      const name = window.prompt('请输出标签名');
-      if (name) {
-        // TODO
-        // store.createTag(name);
-      }
+  export default class Labels extends mixins(TagHelper) {
+    beforeCreate() {
+      this.$store.commit('fetchTags');
     }
   }
 </script>
@@ -50,7 +51,7 @@
       display: flex;
       align-items: center;
       justify-content: space-between;
-      border-bottom: 1px solid #e6e6e6;
+      border-bottom: 1px solid #E6E6E6;
       svg {
         width: 18px;
         height: 18px;
