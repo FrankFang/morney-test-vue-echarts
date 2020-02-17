ba1e310bcb107f2990949309db60f9cfa884e413
frankfang <frankfang1990@gmail.com>
2020-02-17 16:12:17 +0800

    修复刷新变成 404 的 bug

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
-import store2 from '@/store/index2.ts'
 
 Vue.config.productionTip = false;
 
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -32,28 +32,28 @@
 
     created() {
       const id = this.$route.params.id;
+      console.log(id);
+      this.$store.commit('fetchTags');
       this.$store.commit('setCurrentTag', id);
       if (!this.tag) {
+        console.log('no tag');
         this.$router.replace('/404');
+      } else {
+        console.log('has tag');
       }
     }
 
     update(name: string) {
       if (this.tag) {
-        // TODO
-        // store.updateTag(this.tag.id, name);
+        this.$store.commit('updateTag', {
+          id: this.tag.id, name
+        });
       }
     }
 
     remove() {
       if (this.tag) {
-        // TODO
-        return;
-        // if (store.removeTag(this.tag.id)) {
-        //   this.$router.back();
-        // } else {
-        //   window.alert('删除失败');
-        // }
+        this.$store.commit('removeTag', this.tag.id);
       }
     }
 
