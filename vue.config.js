b7748a32940b043771c87d5cbbc1868e93ac8e0b
frankfang <frankfang1990@gmail.com>
2020-02-21 04:03:29 +0800

    部署网站到 morney-3-website

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,18 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/morney-3-website/'
+    : '/',
   lintOnSave: false,
-  chainWebpack: config =>{
+  chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons')
 
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
