bcdceea2126b22e1763fdd03df3528a1ef0481b6
frankfang <frankfang1990@gmail.com>
2020-02-21 04:12:51 +0800

    保证用户用手机打开页面，否则就提示用户

--- src/main.ts
+++ src/main.ts
@@ -24,3 +24,17 @@ window.onload = function () {
     window.scrollTo(0, 200);
   }, 0);
 };
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开本页面，以保证浏览效果');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
+
+
