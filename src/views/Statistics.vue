e965ee25a2db8bc0fc1d1e5420106f7b4df612bd
frankfang <frankfang1990@gmail.com>
2020-07-13 05:42:28 +0800

    重构 x y

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,7 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <div class="chart-wrapper" ref="chartWrapper">
-      <Chart class="chart" :options="x"/>
+      <Chart class="chart" :options="chartOptions"/>
     </div>
     <ol v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
@@ -65,7 +65,7 @@
       }
     }
 
-    get y() {
+    get keyValueList() {
       const today = new Date();
       const array = [];
       for (let i = 0; i <= 29; i++) {
@@ -76,13 +76,13 @@
           createdAt: dateString
         });
         array.push({
-          date: dateString, value: found ? found.amount : 0
+          key: dateString, value: found ? found.amount : 0
         });
       }
       array.sort((a, b) => {
-        if (a.date > b.date) {
+        if (a.key > b.key) {
           return 1;
-        } else if (a.date === b.date) {
+        } else if (a.key === b.key) {
           return 0;
         } else {
           return -1;
@@ -91,9 +91,9 @@
       return array;
     }
 
-    get x() {
-      const keys = this.y.map(item => item.date);
-      const values = this.y.map(item => item.value);
+    get chartOptions() {
+      const keys = this.keyValueList.map(item => item.key);
+      const values = this.keyValueList.map(item => item.value);
       return {
         grid: {
           left: 0,
